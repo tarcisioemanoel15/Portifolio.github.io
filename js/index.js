6ef5dc2cb5bac9b42f0968be84fca1fc759d28a6
tarcisioemanoel15 <caninosbrancos10013@gmail.com>
2021-05-14 17:46:57 -0300

    relogio

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-alert('Site Não terminado');
+//Relogio
+function relogio(){
+function relogio(){
+    function zero(num){
+        return num >= 10 ? num : '0'+num;
+    }
+    const horass = document.querySelector('div#rel'); 
+    const data = new Date();
+    const hor = zero(data.getHours());
+    const min = zero(data.getMinutes());
+    const seg = zero(data.getSeconds());
+    const horas = `${hor}:${min}:${seg}`;
+    return horass.innerHTML= horas;
+}
+setInterval(relogio, 1000);
+}
+relogio();    
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
