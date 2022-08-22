80cf867f4af667c703f4d06cb8ea1b2b91b84b16
Tarcisio <caninosbrancos10031@gmail.com>
2022-08-22 19:16:56 -0300

    comit

--- paginas/js/formulario.js
+++ paginas/js/formulario.js
@@ -1,21 +1,68 @@
-
-const formulario = document.querySelector("form");
-
-
-formulario.addEventListener('submit', e => {
+function inicia() {
+  selctElemunto();
+}
 
 
-  for (const campo of formulario.querySelectorAll('input')) {
-    const label = campo.previousElementSibling.innerHTML;
 
-    if (campo.value === '') {
-      mensageError(campo, `Campo ${label} não dode ficar vazio`);
+
+function selctElemunto() {
+  const formulario = document.querySelector("form");
+
+  formulario.addEventListener('submit', e => {
+    e.preventDefault();
+
+    for (const erro of document.querySelectorAll(".erro")) {
+      erro.remove();
     }
 
+    for (const campo of formulario.querySelectorAll('input')) {
+      const label = campo.previousElementSibling.innerHTML;
+
+      if (campo.value === '') {
+        mensageError(campo, `Campo ${label} não pode ficar vazio`);
+      }
+
+
+      if (campo.classList.contains('idade')) {
+        campoIdade(campo);
+      }
+
+
+
+
+
+
+
+
+    }
+
+  })
+
+}
+
+
+function campoIdade(campo) {
+  const idade = document.querySelector(".idade");
+
+  if (idade.value) {
+
+    if (idade.value >= 0) {
+      if (idade.value <= 17) {
+        return mensageError(campo, "idade tem que ser maior que 18");
+      } else if (idade.value >= 80) {
+        return mensageError(campo, "idade tem que ser menor que 80");
+      } else {
+        return idade.value;
+      }
+    }
+    return;
   }
 
+}
 
-})
+
+
+
 
 
 
@@ -27,3 +74,10 @@ function mensageError(campo, msg) {
 }
 
 alert('ainda não terminado iniciando validação de campo')
+
+
+
+
+
+
+inicia();
