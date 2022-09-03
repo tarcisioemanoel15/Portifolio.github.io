047c74436cd7a5f7b800cc8f2a7051fd7bef4730
Tarcisio <caninosbrancos10031@gmail.com>
2022-09-03 13:03:11 -0300

    finalizado estolo hbilidades

---
+++ paginas/habilidades.js
@@ -0,0 +1,25 @@
+
+class Capclick {
+  constructor() {
+    this.container = document.querySelector('.container');
+    this.inicia();
+  }
+
+
+  inicia() {
+
+
+
+
+
+
+  }
+
+
+
+
+
+}
+
+const capClick = new Capclick();
+
